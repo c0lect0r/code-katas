9668dea97f52a86024519ff8ae5b91251884597c
c0lect0r <thomas.luchte@gmx.de>
2017-11-19 11:14:10 +0100

    rwre

---
+++ src/main/java/de/c0lect0r/codekatas/maventest/Main.java
@@ -0,0 +1,10 @@
+package de.c0lect0r.codekatas.maventest;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/test/java/de/c0lect0r/codekatas/maventest/mainTest.java
@@ -0,0 +1,15 @@
+package de.c0lect0r.codekatas.maventest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class mainTest {
+
+	@Test
+	public void test() {
+		Main main = new Main();
+		//fail("Not yet implemented");
+	}
+
+}
